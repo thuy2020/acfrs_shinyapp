08c78addd3e43f7373a93cdf58aff18d68e11bae
thuy <thuyvei@gmail.com>
2022-07-08 09:57:26 -0700

    building full site structure

--- server.R
+++ server.R
@@ -267,52 +267,18 @@ function(input, output, session) {
           legend.position = "bottom")
   })
   
+########Begin - Home Tab
  
 output$all_entities_liab_rev_ratio <- renderPlotly({
 
-  
-  state <- read_csv("data/states.csv") %>% select(state, lib_rev_ratio) %>% 
-    mutate(lib_rev_ratio = round(lib_rev_ratio)) %>% 
-    rename(State = state, `Liability-Renenue Ratio` = lib_rev_ratio) 
-  
-  city <- read_csv("data/cities.csv") %>% select(state, city, lib_rev_ratio) %>% 
-    mutate(lib_rev_ratio = round(lib_rev_ratio)) %>% 
-    rename(City = city, `Liability-Renenue Ratio` = lib_rev_ratio)
-  
-  county <- read_csv("data/counties.csv") %>% arrange(desc(population)) %>% slice(1:100) %>% 
-    select(state, name, lib_rev_ratio) %>% 
-    mutate(lib_rev_ratio = round(lib_rev_ratio)) %>% 
-    rename(County = name, `Liability-Renenue Ratio` = lib_rev_ratio) 
-  liab_rev_ratio_allentities <- state %>% 
-    ggplot(aes(State, `Liability-Renenue Ratio`)) +
-    geom_point(color = "purple", 
-               size = 10, alpha = .2) +
-    
-    #2
-    geom_point(aes(City, `Liability-Renenue Ratio`),
-               data = city,
-               color = "#E7B800", 
-               size = 10, alpha = .2) +
-    
-    geom_point(aes(County, `Liability-Renenue Ratio`),
-               data = county,
-               color = "#00AFBB", 
-               size = 10, alpha = .2) +
-    labs(
-      x = "",
-      y = "") +
-    scale_y_continuous(labels = scales::percent_format(suffix = "%", scale = 1)) +
-    
-    theme(axis.text.x  = element_text(color="white"),
-          axis.ticks = element_blank(),
-          panel.background = element_blank()
-    ) 
-  
-  ggplotly(liab_rev_ratio_allentities)
+  all_entities_liab_rev_ratio <- readRDS("all_entities_liab_rev_ratio.RDS")
   
 })
 
-# Data tab
+
+#######---------------
+
+####### Data tab
 updateSelectInput(session,
                   "select_entity_type",
                   choices = unique(acfrs$category))
@@ -324,5 +290,19 @@ updateSelectInput(session,
 updateSelectInput(session,
                   "select_indicator",
                   choices = colnames(acfrs)[7:20])
+
+###Begin - entity explorer -----------------------------------------
+
+comparison_state_mean_state_gov <- readRDS("comparison_state_mean_state_gov.RDS")
+
+output$state_gov_vs_mean_chart <- renderPlot({
+  comparison_state_mean_state_gov
   
+})
+
+###End - entity explorer -----------------------------------------
+
+
+
+
 }
--- ui.R
+++ ui.R
@@ -6,6 +6,7 @@ library(shinythemes)
 library(shinyWidgets)
 library(htmltools)
 library(fresh)
+
 navbarPage(
  title = "Government Finance Data Transparency", 
  header = use_theme(
@@ -18,82 +19,106 @@ navbarPage(
        default_color = "#FFFFFF",
        default_link_color = "#FFFFFF",
        default_link_active_color = "#FFFFFF"
-     
-   )
- )),
- 
+   ))),
+ ########## Begin - Home
   tabPanel("Home",
-          
            fluidPage(
-             wellPanel("Debt bubbles in 50 states and DC, top 100 counties, and top 100 cities", 
-                       style = "background: white"), 
+             wellPanel("Debt bubbles in 50 states and DC, top 100 counties, and top 100 cities"), 
              plotlyOutput("all_entities_liab_rev_ratio"),
              plotOutput("top4_cities"),
              plotlyOutput("revevue_per_cap_50cities_chart"))),
   
-  
+  ########## Begin - Analysis
   navbarMenu("Analysis",
    tabPanel("States",
       fluidPage(
         plotOutput("states_ratio_chart", width = "80%"), 
         plotOutput("states_chart"), 
-      
     )),
-  
    tabPanel("Cities",
       fluidPage(
         plotOutput("cities_chart"),
         DTOutput("cities")
     )),
-   
    tabPanel("Counties",
             fluidPage(
               plotOutput("counties_chart"),
               DTOutput("counties")
             )),
-   
    tabPanel("Districts",
             fluidPage(
               plotOutput("")
-            ))
-  ),
-  tabPanel(
-    "Data",
-    
-    fluidPage(
-      wellPanel("View & Download the data to your machine"),
+            ))),
+ #########End - Analysis
+ ####### Begin - Data
+  navbarMenu("Data",
+    ##1
+    tabPanel("Data Exploration",
+              # Chart part----------------         
+            fluidPage(
+              titlePanel("Data Exploration"),
+              fluidRow(column(selectInput("select_entity_type", "Select a Type of Government",
+                                          choices = NULL), width = 4),
+              ##
+              sidebarLayout(
+                sidebarPanel(
+                  textInput("name", "Enter Name", "State of Arizona")
+                ),
+                mainPanel(
+                  tabsetPanel(
+                    tabPanel("Plot", plotOutput("state_gov_vs_mean_chart")),
+                    tabPanel("Table", DTOutput(""))
+                  )))
+              )),
       
-      
-      
-      fluidRow(column(selectInput("select_entity_type", "Select an Entity Type",
-                         choices = NULL), width = 4),
-               column(selectInput("select_entity", "Select an Entity",
-                                  choices = NULL), width = 4),
-              column(selectInput("select_indicator","Select an Indicator",
-                          choices = NULL), width = 4)),
-
-      #plotOutput(""),
-      
-      downloadButton(
-        "download_data",
-        "Download data"
-      ),
-      hr(),
-      DTOutput("acfrs")
-    )
-    
-  ),
-  tabPanel(
-    "Blogs",
-    fluidPage()
-),
-
-tabPanel(
-  "About",
+            # Data part----------------
+          fluidPage(
+            # fluidRow(column(selectInput("select_entity_type", "Select a Type of Government",
+            #                    choices = NULL), width = 4),
+            #         column(selectInput("select_indicator","Select an Indicator",
+            #                     choices = NULL), width = 4)),
+            wellPanel("Search by name of state, entity or category"),
+            downloadButton(
+              "download_data", "Download data"
+            ),
+            hr(),
+            DTOutput("acfrs")
+          )),
+  
+  ##2
+  tabPanel("Codebook",
+           fluidPage(
+             tags$h3("Description of each field in the dataset"),
+             p("Category: Type of Government:  General Purpose (e.g.  a state, county, city, town, village or borough), School District, Special District, Community College District or Public Higher Education"))
+           ),
+  ##3
+  tabPanel("Methodology",
+           fluidPage(
+             tags$h3("How we collected the data"),
+           )
+           )),
+  
+  ########## End - Data
+ ########Begin - About
+ navbarMenu("About",
+    tabPanel("Government Finance Data Transparency Project",
+  
   fluidPage(
-    wellPanel("This is an awesome project!")
-  )
-),
+    tags$h3("What is Government Finance Data Transparency Project?"),
+    p("A very important project!"),
+    
+  )),
+
+  tabPanel("Q&A",
+           
+           fluidPage(
+             tags$h3("Answers to many of your questions"),
+             p("A legit answer."),
+           )  
+           )),
+  
+ ########End - About
+  
 collapsible = TRUE
 
 )
